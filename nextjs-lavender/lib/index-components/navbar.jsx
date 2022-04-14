7965e056bf09a7eee9fff27749ca42cdea077ddb
No767 <73260931+No767@users.noreply.github.com>
2022-04-14 00:08:41 -0700

    Do Not Pass Children as Props for Navbar
    Fixes the menu on the navbar being broken

--- nextjs-lavender/lib/index-components/navbar.jsx
+++ nextjs-lavender/lib/index-components/navbar.jsx
@@ -13,94 +13,92 @@ import {
 
 export function Navbar({}) {
   return (
-    <AppShell
-      header={
-        <Header height={60} padding="xs" fixed="true">
-          <Group
-            spacing="xl"
-            position="center"
-            sx={{ paddingTop: 10, paddingBottom: 10 }}
+    <AppShell>
+      <Header height={60} padding="xs" fixed="true">
+        <Group
+          spacing="xl"
+          position="center"
+          sx={{ paddingTop: 10, paddingBottom: 10 }}
+        >
+          <Button
+            variant="outline"
+            style={{
+              width: 150,
+              color: "#FFFFFF",
+            }}
+            leftIcon={<SiGithub />}
+            component="a"
+            href="https://github.com/No767/Lavender"
           >
-            <Button
-              variant="outline"
-              style={{
-                width: 150,
-                color: "#FFFFFF",
-              }}
-              leftIcon={<SiGithub />}
-              component="a"
-              href="https://github.com/No767/Lavender"
-            >
-              GitHub
-            </Button>
-            <Menu
-              control={
-                <Button
-                  variant="outline"
-                  style={{
-                    width: 150,
-                  }}
-                  leftIcon={<BsBook />}
-                  sx={{ color: "#FFFFFF" }}
-                >
-                  <Text size="sm">Portfolio</Text>
-                </Button>
-              }
+            GitHub
+          </Button>
+          <Menu
+            control={
+              <Button
+                variant="outline"
+                style={{
+                  width: 150,
+                }}
+                leftIcon={<BsBook />}
+                sx={{ color: "#FFFFFF" }}
+              >
+                <Text size="sm">Portfolio</Text>
+              </Button>
+            }
+            placement="center"
+            size="md"
+          >
+            <Menu.Item
               placement="center"
-              size="md"
+              icon={<MdHome />}
+              component="a"
+              href="/"
             >
-              <Menu.Item
-                placement="center"
-                icon={<MdHome />}
-                component="a"
-                href="/"
-              >
-                Site Home
-              </Menu.Item>
-              <Menu.Item
-                placement="center"
-                icon={<MdHome />}
-                component="a"
-                href="/home"
-              >
-                Portfolio Home
-              </Menu.Item>
-              <Menu.Item
-                placement="center"
-                icon={<MdOutlineInfo />}
-                component="a"
-                href="/about"
-              >
-                About
-              </Menu.Item>
-              <Menu.Item
-                placement="center"
-                icon={<BsEggFried />}
-                component="a"
-                href="/recipe"
-              >
-                Recipe
-              </Menu.Item>
-              <Menu.Item
-                placement="center"
-                icon={<MdVideogameAsset />}
-                component="a"
-                href="/game-lab"
-              >
-                Game Lab
-              </Menu.Item>
-              <Menu.Item
-                placement="center"
-                icon={<MdSmartphone />}
-                component="a"
-                href="/app-lab"
-              >
-                App Lab
-              </Menu.Item>
-            </Menu>
-          </Group>
-        </Header>
-      }
-    ></AppShell>
+              Site Home
+            </Menu.Item>
+            <Menu.Item
+              placement="center"
+              icon={<MdHome />}
+              component="a"
+              href="/home"
+            >
+              Portfolio Home
+            </Menu.Item>
+            <Menu.Item
+              placement="center"
+              icon={<MdOutlineInfo />}
+              component="a"
+              href="/about"
+            >
+              About
+            </Menu.Item>
+            <Menu.Item
+              placement="center"
+              icon={<BsEggFried />}
+              component="a"
+              href="/recipe"
+            >
+              Recipe
+            </Menu.Item>
+            <Menu.Item
+              placement="center"
+              icon={<MdVideogameAsset />}
+              component="a"
+              href="/game-lab"
+            >
+              Game Lab
+            </Menu.Item>
+            <Menu.Item
+              placement="center"
+              icon={<MdSmartphone />}
+              component="a"
+              href="/app-lab"
+            >
+              App Lab
+            </Menu.Item>
+          </Menu>
+        </Group>
+      </Header>
+    </AppShell>
   );
 }
